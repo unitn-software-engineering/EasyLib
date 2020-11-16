b256af6767c13580e29066101a711bef898f5055
Marco Robol <marco89mr@gmail.com>
2020-11-17 00:29:50 +0100

    remove db url

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ module.exports = {
 
 	superSecret: process.env.SUPER_SECRET || 'is2lab2020',
 	database: {
-		uri: process.env.DB_URL //|| 'mongodb+srv://admin:GiiiYyXyPkj2hiI1@cluster0.f9mww.mongodb.net/Cluster0?retryWrites=true&w=majority',
+		uri: process.env.DB_URL
 	}
 	
 };
