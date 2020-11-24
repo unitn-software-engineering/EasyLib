b762c4c81ccde6ebe9d7ddca18bca76ffe9baec7
Marco Robol <marco.robol@unitn.it>
2020-11-24 11:16:11 +0100

    fix query exec()

--- app/authentication.js
+++ app/authentication.js
@@ -7,12 +7,12 @@ const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 // ---------------------------------------------------------
 // route to authenticate and get a new token
 // ---------------------------------------------------------
-router.post('.', async function(req, res) {
+router.post('', async function(req, res) {
 	
 	// find the user
 	let user = await Student.findOne({
 		email: req.body.email
-	}).exec;
+	}).exec();
 	
 	// user not found
 	if (!user) {
