6e624219da9418985e490c4ec9fd91b90ac17c3a
Marco Robol <marco89mr@gmail.com>
2022-05-14 14:15:25 +0200

    process.env.FRONTEND configuration

--- app/app.js
+++ app/app.js
@@ -48,7 +48,9 @@ app.use(cors())
 /**
  * Serve front-end static files
  */
-app.use('/', express.static('static'));
+app.use('/', express.static(process.env.FRONTEND || 'static'));
+// If process.env.FRONTEND folder does not contain index.html then use the one from static
+app.use('/', express.static('static')); // expose also this folder
 
 
 
